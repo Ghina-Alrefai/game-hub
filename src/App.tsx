e17f485043ccd9cdc61098c9ade854e23c4c689e
Ghina Alrefai <ghina.alrefai12@gmail.com>
2024-09-14 17:29:35 +0300

    bulid layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Grid, GridItem, Box, Show } from "@chakra-ui/react"; // Import Box for conditional rendering
+import { useMediaQuery } from '@chakra-ui/react'; // Import media query hook
 
 function App() {
   
-
-  return <Button colorScheme='blue'>Button</Button>
+  return (
+    <Grid templateAreas={{
+      base: `"nav " "main"`,
+      lg: `"nav nav" "asid main"`
+    }}>
+      <GridItem area="nav" bg="coral">Nav</GridItem>
+      <Show above="lg">
+      <GridItem area="asid" bg="gold">asid</GridItem>
+      </Show>
+      <GridItem area="main" bg="blue">main</GridItem>
+  
+   
+    </Grid>
+  );
 }
 
-export default App
+export default App;
