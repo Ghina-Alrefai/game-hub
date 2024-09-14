38a4ad61351cd9f839be6b764bbc73175a060d60
Ghina Alrefai <ghina.alrefai12@gmail.com>
2024-09-14 18:20:19 +0300

    nave bare

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Grid, GridItem, Box, Show } from "@chakra-ui/react"; // Import Box for conditional rendering
-import { useMediaQuery } from '@chakra-ui/react'; // Import media query hook
+//import { useMediaQuery } from '@chakra-ui/react'; // Import media query hook
+import { NavBar } from "./components/NavBar";
 
 function App() {
   
@@ -8,7 +9,9 @@ function App() {
       base: `"nav " "main"`,
       lg: `"nav nav" "asid main"`
     }}>
-      <GridItem area="nav" bg="coral">Nav</GridItem>
+      <GridItem area="nav" >
+        <NavBar />
+      </GridItem>
       <Show above="lg">
       <GridItem area="asid" bg="gold">asid</GridItem>
       </Show>
